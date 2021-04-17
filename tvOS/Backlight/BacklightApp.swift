3221856d59f522bd73721271cc7a9c43c89ce0ab
Dan Appel <dan.appel00@gmail.com>
2021-04-16 22:31:33 -0700

    Add tvOS scaffolding

---
+++ tvOS/Backlight/BacklightApp.swift
@@ -0,0 +1,17 @@
+//
+//  BacklightApp.swift
+//  Backlight
+//
+//  Created by Dan Appel on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BacklightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
